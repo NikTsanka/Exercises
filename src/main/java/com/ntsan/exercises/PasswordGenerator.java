1e9d74ffd4458a370c90616d508d3b5a2abf821b
NikTsanka <nik.tsanka@gmail.com>
2020-12-17 00:21:33 +0400

    adding password generator exercises

--- src/main/java/com/ntsan/exercises/PasswordGenerator.java
+++ src/main/java/com/ntsan/exercises/PasswordGenerator.java
@@ -9,7 +9,7 @@ public class PasswordGenerator {
 
     public static String randomPassword() {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("pleace enter your password length: ");
+        System.out.print("please enter your password length: ");
         int length = scanner.nextInt();
         String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%";
         char[] password = new char[length];
